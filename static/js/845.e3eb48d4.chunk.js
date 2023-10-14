8e4782216b599eaa8b6db00811e3f77a90dac845
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-10-14 11:45:26 +0000

    Deploying to gh-pages from @ OleksiiVTS/final-project-frontend@67ef435b4920b59aa60ddd080a7b9068661323f5 🚀

---
+++ static/js/845.e3eb48d4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project_frontend=self.webpackChunkfinal_project_frontend||[]).push([[845],{845:function(n,e,r){r.r(e);r(791);var t=r(329);e.default=function(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:" MainLayout"})})}}}]);
+//# sourceMappingURL=845.e3eb48d4.chunk.js.map
