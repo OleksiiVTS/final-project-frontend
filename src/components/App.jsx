dc328865762db85552c6e333def76cafcc7fe687
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-10-14 13:34:29 +0300

    Add - authOperations, pages, lazy import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,35 @@
-import React, { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
-// import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
 // import { Circles } from 'react-loader-spinner';
 // import css from '../components/Loader/Loader.module.css';
 
 // import PrivateRoute from './PrivateRoute';
 // import PublicRoute from './PublicRoute';
 
-const NotFound = lazy(() => import('../pages/NotFound.jsx'));
+// const MainLayout = lazy(() =>
+//   import('../components/MainLayout/MainLayout.jsx')
+// );
+// const MainPage = lazy(() => import('../pages/Public/MainPage/MainPage.jsx'));
+// const RegisterPage = lazy(() =>
+//   import('../pages/Public/RegisterPage/RegisterPage.jsx')
+// );
+// const LoginPage = lazy(() => import('../pages/Public/LoginPage/LoginPage.jsx'));
+// const AccountPage = lazy(() =>
+//   import('../pages/Private/AccountPage/AccountPage.jsx')
+// );
+// const CalendarPage = lazy(() =>
+//   import('../pages/Private/CalendarPage/CalendarPage.jsx')
+// );
+// const StatisticsPage = lazy(() =>
+//   import('../pages/Private/StatisticsPage/StatisticsPage.jsx')
+// );
+// const NotFound = lazy(() => import('../pages/NotFound/NotFound.jsx'));
 
 export const App = () => {
   return (
     // <Circles height="80" width="80" color="#4d78a9" wrapperClass={css.loader} />
-    <Routes>
-      <Route>
-        <div>
-          <h1>New project!</h1>
-        </div>
-        <Route path="*" element={<NotFound />}></Route>
-      </Route>
-    </Routes>
+    <div>
+      <h1>APP starting!</h1>
+    </div>
   );
 };
