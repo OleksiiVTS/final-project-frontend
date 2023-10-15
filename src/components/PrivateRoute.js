43a4159cd06ff25ae073cefa3487887b88fcbf22
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-10-15 22:12:21 +0200

    fixed routes - test version

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { selectLoggedIn } from 'redux/auth/authSelectors';
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
+// import { selectLoggedIn } from 'redux/auth/authSelectors';
 
-const PrivateRoute = ({ children, redirectTo = '/' }) => {
-  const logedIn = useSelector(selectLoggedIn);
+// const PrivateRoute = ({ children, redirectTo = '/' }) => {
+//   const logedIn = useSelector(selectLoggedIn);
 
-  return logedIn ? children : <Navigate to={redirectTo} />;
-};
 
-export default PrivateRoute;
+//   return logedIn ? children : <Navigate to={redirectTo} />;
+// };
+
+// export default PrivateRoute;
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectLoggedIn } from 'redux/auth/authSelectors.js';
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { selectLoggedIn } from 'redux/auth/authSelectors.js';
 
-const PublicRoute = ({ children }) => {
-  const isAuthenticated = useSelector(selectLoggedIn);
+// const PublicRoute = ({ children }) => {
+//   const logedIn = useSelector(selectLoggedIn);
 
-  if (isAuthenticated) {
-    return <Navigate to="/account" />;
-  }
 
-  return children;
-};
 
-export default PublicRoute;
+//   if (logedIn) {
+//     return <Navigate to="/account" />;
+//   }
+
+//   return children;
+// };
+
+// export default PublicRoute;
