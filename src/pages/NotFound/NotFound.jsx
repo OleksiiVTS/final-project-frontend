e5c87e22cca576bae3d2521ada09749143856d1c
IraMira333 <irinat333@gmail.com>
2023-10-18 00:45:42 +0300

    Finished 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import IMG404 from '../../components/Pictures/goose_404.jpeg';
+import IMG404 from '../../components/Pictures/goose_404.png';
 import {
   BackButton,
   BackHome,
+  BoxNotFoundPage,
+  ContainerNotFoundPage,
   Image,
   ImagePosition,
-  Title,
+  Text404,
+  TitleNotFoundPage,
 } from './NotFound.styled';
 
 const NotFound = () => {
   return (
-    <>
-      <ImagePosition>
-        <Title>4</Title>
-        <Image src={IMG404} alt="Goose" />
-        <Title>4</Title>
-      </ImagePosition>
-      <BackButton>
-        <BackHome>Back to home</BackHome>
-      </BackButton>
-    </>
+    <ContainerNotFoundPage>
+      <BoxNotFoundPage>
+        <ImagePosition>
+          <TitleNotFoundPage>4</TitleNotFoundPage>
+          <Image src={IMG404} alt="Goose" />
+          <TitleNotFoundPage>4</TitleNotFoundPage>
+        </ImagePosition>
+        <Text404>
+          We’re sorry, the page you requested could not be found. Please go back
+          to the homepage.
+        </Text404>
+        <BackButton>
+          <BackHome to="/">Back to home</BackHome>
+        </BackButton>
+      </BoxNotFoundPage>
+    </ContainerNotFoundPage>
   );
 };
 
--- src/pages/NotFound/NotFound.styled.jsx
+++ src/pages/NotFound/NotFound.styled.jsx
@@ -1,15 +1,42 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
-export const Title = styled.h1`
+export const ContainerNotFoundPage = styled.div`
+  display: block;
+  width: 100vh;
+  height: 100vh;
+  background-color: #f7f6f9;
+`;
+
+export const BoxNotFoundPage = styled.div`
+  width: 281px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+
+  @media (min-width: 768px) {
+    width: 503px;
+  }
+`;
+
+export const TitleNotFoundPage = styled.h1`
   font-family: Inter;
   font-size: 100px;
   font-weight: 700;
-  line-height: 150px;
+  line-height: 1.5;
   letter-spacing: 0em;
   margin: 0;
   text-align: left;
   color: #3e85f3;
+  text-shadow: 0px 9.4px 57.7px rgba(0, 0, 0, 0.04),
+    0px 47px 355px rgba(0, 0, 0, 0.07);
+
+  @media (min-width: 768px) {
+    font-size: 200px;
+    line-height: 1.25; /* 125% */
+  }
 `;
 
 export const ImagePosition = styled.div`
@@ -19,18 +46,17 @@ export const ImagePosition = styled.div`
   max-width: 252px;
   margin-left: auto;
   margin-right: auto;
+
+  @media (min-width: 768px) {
+    max-width: 503px;
+  }
 `;
 export const Image = styled.img`
-  /* @media screen and (max-width: 1439px) {
-    display: none;
-  } */
-
   transform: rotate(-31.56deg);
 
-  @media screen and (max-width: 375px) {
-    width: 81px;
-    height: 188px;
-  }
+  width: 81px;
+  height: 188px;
+
   @media (min-width: 768px) {
     width: 178px;
     height: 414px;
@@ -39,19 +65,36 @@ export const Image = styled.img`
   }
 `;
 
+export const Text404 = styled.p`
+  color: rgba(17, 17, 17, 0.7);
+
+  text-align: center;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.29;
+  width: 281px;
+  margin-top: 28px;
+
+  margin-left: auto;
+  margin-right: auto;
+  @media screen and (min-width: 768px) {
+    width: 387px;
+    margin-top: 0px;
+  }
+`;
+
 export const BackButton = styled.button`
-  width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 8px;
-  padding: 14px 0;
+  margin-top: 24px;
+  padding: 14px 32px;
   border-radius: 16px;
   border: 1px solid transparent;
   box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
-  font-size: 14px;
+
   background-color: #3e85f3;
-  color: #ffffff;
+
   margin-left: auto;
   margin-right: auto;
   cursor: pointer;
@@ -64,23 +107,23 @@ export const BackButton = styled.button`
   }
 
   @media screen and (min-width: 768px) {
-    margin-top: 30px;
-    font-size: 18px;
+    margin-top: 32px;
+    padding: 16px 48px;
   }
 `;
 export const BackHome = styled(NavLink)`
-  display: block;
-  margin-top: 18px;
   text-align: center;
   font-family: Inter;
-  font-size: 18px;
+  font-size: 14px;
   font-weight: 600;
-  line-height: 1.17;
+  line-height: 1.29;
   color: #ffffff;
-
+  letter-spacing: -0.28px;
   text-decoration: none;
 
   @media screen and (min-width: 768px) {
     font-size: 18px;
+    line-height: 1.33;
+    letter-spacing: -0.36px;
   }
 `;
