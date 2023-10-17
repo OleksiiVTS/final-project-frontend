abdd7892b883f3809fad1bf6f5f2f140625b9e1a
vladislav-slobodenjuk <vladjun@gmail.com>
2023-10-17 20:17:42 +0300

    replaced currentDate mock at AddTaskBtn; TaskModal for addTask button at ColumnHeadBar; replaced user and task mocks at TaskToolbar

--- src/components/ChoosedDay/TasksColumnItem/ColumnTasksList/TaskColumnCard/TaskToolbar/TaskToolbar.jsx
+++ src/components/ChoosedDay/TasksColumnItem/ColumnTasksList/TaskColumnCard/TaskToolbar/TaskToolbar.jsx
@@ -1,41 +1,20 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import TaskModal from 'components/TaskModal/TaskModal';
 import { StyledTaskToolbar } from './TaskToolbar.styled';
 
-import sprite from '../../../../../Pictures/sprite.svg';
-import { useState } from 'react';
-import TaskModal from 'components/TaskModal/TaskModal';
-// import { useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/authSelectors';
 
-const TaskToolbar = ({ priority, id }) => {
+import sprite from '../../../../../Pictures/sprite.svg';
+
+const TaskToolbar = ({ task }) => {
   const [showModal, setShowModal] = useState(false);
   const [deleteTask, setDeleteTask] = useState(false);
 
-  // const user = useSelector('user')
-  const user = {
-    _id: { $oid: '6529476a97b7633982504c1e' },
-    email: 'vlad4@mail.com',
-    password: '$2a$10$/LQqZa97FIpqrzsuN0fhi.yzcowlVFbq5TpxWaV5iitOMgexu.lVi',
-    subscription: 'starter',
-    avatarURL:
-      'http://res.cloudinary.com/dnd9pw6nh/image/upload/v1697204074/avatarUser/scymdkr4ztejgfzcj8qh.jpg',
-    public_id: 'avatarUser/scymdkr4ztejgfzcj8qh',
-    verify: true,
-    name: 'vlad',
-    verificationToken: ' ',
-    createdAt: { $date: { $numberLong: '1697204074420' } },
-    updatedAt: { $date: { $numberLong: '1697315175368' } },
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1Mjk0NzZhOTdiNzYzMzk4MjUwNGMxZSIsImlhdCI6MTY5NzMxNTE3NSwiZXhwIjoxNjk3Mzk3OTc1fQ.c3V0oMP1jeWgx4RiyHPD5pJot0DdtNl2Eus3B-juXOw',
-  };
-
-  const taskFromTheProps = {
-    _id: '2222222',
-    title: 'my task',
-    start: '09:00',
-    end: '13:00',
-    priority: 'medium',
-    category: 'to-do',
-  };
+  const user = useSelector(selectUser);
   const { avatarURL, name } = user;
+  const { priority } = task;
 
   const priorityColors = {
     low: '#72C2F8',
@@ -43,15 +22,15 @@ const TaskToolbar = ({ priority, id }) => {
     high: '#EA3D65',
   };
 
-  const onCategoryChange = e => {
-    console.log(`change category for id ${id}`);
+  const onCategoryChange = () => {
+    console.log(`change category for id ${task._id}`);
   };
 
-  const onEditClick = e => {
+  const onEditClick = () => {
     setShowModal(true);
   };
 
-  const onDeleteClick = e => {
+  const onDeleteClick = () => {
     setShowModal(true);
     setDeleteTask(true);
   };
@@ -94,7 +73,7 @@ const TaskToolbar = ({ priority, id }) => {
       </div>
       {showModal && (
         <TaskModal
-          task={taskFromTheProps}
+          task={task}
           closeModal={closeModal}
           deleteTask={deleteTask}
         />
