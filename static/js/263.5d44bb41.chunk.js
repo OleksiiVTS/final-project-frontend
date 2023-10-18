fab919e468c4a0ea029f4a333e6b57e914c6aa9c
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-10-18 19:08:04 +0000

    Deploying to gh-pages from @ OleksiiVTS/final-project-frontend@3bee4dd84510b3c59ed88e84f524953a279e42b1 🚀

---
+++ static/js/263.5d44bb41.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project_frontend=self.webpackChunkfinal_project_frontend||[]).push([[263],{263:function(n,e,t){t.r(e);var r=t(9841),c=(t(2791),t(3329));e.default=function(){return(0,c.jsx)(r.Z,{children:(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:" StatisticsPage"})})})}}}]);
+//# sourceMappingURL=263.5d44bb41.chunk.js.map
