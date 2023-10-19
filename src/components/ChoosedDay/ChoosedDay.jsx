5e45835378ff2f9ee62a942c8764eed1a0eff8b4
vladislav-slobodenjuk <vladjun@gmail.com>
2023-10-19 19:03:42 +0300

    connected state as data source; fixed deleteTask Thunk _id parameter; fixed addTask state reducer

--- src/components/ChoosedDay/ChoosedDay.jsx
+++ src/components/ChoosedDay/ChoosedDay.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { selectTasks } from 'redux/task/taskSelectors';
+import { useSelector } from 'react-redux';
+import { selectTasks } from 'redux/task/taskSelectors';
 
 import { StyledChoosedDay } from './ChoosedDay.styled';
 import TasksColumnsList from './TasksColumnsList/TasksColumnsList';
@@ -8,88 +8,13 @@ import TasksColumnsList from './TasksColumnsList/TasksColumnsList';
 
 const ChoosedDay = () => {
   const { currentDate } = useParams();
-  console.log('currentDate', currentDate);
+  // console.log('currentDate', currentDate);
   const day = currentDate.slice(-2);
   // const day = new Date(currentDate).getDate();
   // const day = format(currentDate, 'dd');
-  console.log('day', day);
+  // console.log('day', day);
 
-  // const emptymonthTasks = useSelector(selectTasks);
-
-  const monthTasks = [
-    {
-      _id: '65294bc4924f405b17970a7e',
-      title: 'first',
-      start: '14:15',
-      end: '03:00',
-      priority: 'medium',
-      date: '2023-12-31',
-      category: 'done',
-      owner: {
-        $oid: '6529476a97b7633982504c1e',
-      },
-    },
-    {
-      _id: '65294cf6924f405b17970a82',
-      title: 'second',
-      start: '14:15',
-      end: '03:00',
-      priority: 'low',
-      date: '2023-12-31',
-      category: 'to-do',
-      owner: {
-        $oid: '6529476a97b7633982504c1e',
-      },
-    },
-    {
-      _id: '65294d04924f405b17970a85',
-      title: 'third',
-      start: '14:15',
-      end: '03:00',
-      priority: 'high',
-      date: '2023-12-31',
-      category: 'to-do',
-      owner: {
-        $oid: '6529476a97b7633982504c1e',
-      },
-    },
-    {
-      _id: '6529508bbbaee1c2770c5ba9',
-      title: 'fourth',
-      start: '14:15',
-      end: '03:00',
-      priority: 'low',
-      date: '2023-12-31',
-      category: 'done',
-      owner: {
-        $oid: '6529476a97b7633982504c1e',
-      },
-    },
-    {
-      _id: '652ad610673d4d226e29333e',
-      title: 'fifth',
-      start: '15:30',
-      end: '12:12',
-      priority: 'high',
-      date: '2023-12-31',
-      category: 'done',
-      owner: {
-        $oid: '6529476a97b7633982504c1e',
-      },
-    },
-    {
-      _id: '652ad7a68911da03b5124190',
-      title: 'sixth',
-      start: '15:30',
-      end: '12:12',
-      priority: 'medium',
-      date: '2023-12-31',
-      category: 'to-do',
-      owner: {
-        $oid: '6529476a97b7633982504c1e',
-      },
-    },
-  ];
+  const monthTasks = useSelector(selectTasks);
 
   const filterDayTasks = (monthTasks, day) => {
     return monthTasks.filter(task => task.date.slice(-2) === day);
@@ -106,22 +31,6 @@ const ChoosedDay = () => {
     return obj;
   };
   const sortedTasks = CreateSortedTasks(categoryList, dayTasks);
-  // console.log(data);
-
-  // const currentTasks = [[], [], []];
-
-  // dayTasks.forEach(task => {
-  //   if (task.category === categoryList[0]) currentTasks[0].push(task);
-  //   if (task.category === categoryList[1]) currentTasks[1].push(task);
-  //   if (task.category === categoryList[2]) currentTasks[2].push(task);
-  // });
-
-  // const currentTasks = tasksForCurrentDay.reduce((acc, task) => {
-  //   console.log(task.category);
-  //   acc[task.category] = [...acc[task.category], task];
-  //   return acc;
-  // }, {});
-  // console.log(currentTasks);
 
   return (
     <StyledChoosedDay>
