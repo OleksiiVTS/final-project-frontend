425d92f6d053138d1eba9fe7336144cfe4dc1d2a
Rogok-Serg <rogoksv@gmail.com>
2023-10-19 12:38:33 +0300

    Statistics

---
+++ src/components/Statistics/StatisticsChart.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+// import { getTasks } from 'redux/task/tasksAPI';
+
+const StatisticsChart = () => {
+  // const getTasksResp = async interval => {
+  //   const tasksMonth = await getTasks(interval);
+  //   console.log('tasksMonth: ', tasksMonth);
+
+  //   console.log('lengts: ', tasksMonth.length);
+  //   tasksMonth.map(task => console.log(task.category));
+  // };
+  // getTasksResp(2023);
+  // // const mapTask = () => {
+  //   getTasksResp(2023-10).map(task => console.log(task.category));
+
+  // }
+  // mapTask()
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  return (
+    <BarChart width={900} height={600} data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar
+        dataKey="pv"
+        fill="linear-gradient(0deg, #3E85F3 0%, rgba(62, 133, 243, 0.00) 100%)"
+      />
+      <Bar dataKey="uv" fill="#82ca9d" />
+    </BarChart>
+  );
+};
+
+export default StatisticsChart;
