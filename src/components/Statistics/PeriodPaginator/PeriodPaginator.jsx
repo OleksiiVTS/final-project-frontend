b37977efeca1267b90a9170274d4549c5771f731
Rogok-Serg <rogoksv@gmail.com>
2023-10-21 00:29:08 +0300

    Update PeriodPaginator.jsx

--- src/components/Statistics/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/Statistics/PeriodPaginator/PeriodPaginator.jsx
@@ -1,20 +1,29 @@
-import { CalendarToolbar } from 'components/Calendar/common';
+import { PeriodSelector, PeriodTypeSelector } from 'components/Calendar/common';
 import { useSelector } from 'react-redux';
 import { selectTheme } from 'redux/header/headerSlice';
 import { getCurrentDate } from 'utils/calendar';
+// import { getCurrentDate } from 'utils/calendar';
 
-const PeriodPaginator = () => {
+const PeriodPaginator = ({ onClickPrev, onClickNext, type, onChangeType }) => {
   const theme = useSelector(selectTheme);
   const currentDate = getCurrentDate();
 
   return (
     <div>
-      <CalendarToolbar
+      <PeriodSelector
+        onClickPrev={onClickPrev}
+        onClickNext={onClickNext}
+        type={type}
+        date={currentDate}
+        theme={theme}
+      />
+
+      {/* <CalendarToolbar
         // onClickPrev={handlePrev}
         // onClickNext={handleNext}
         today={currentDate}
         theme={theme}
-      />
+      /> */}
     </div>
   );
 };
