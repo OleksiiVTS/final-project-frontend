a2db3c2a46208433d6d6d8695138b1ca8f537d1d
IraMira333 <irinat333@gmail.com>
2023-10-20 14:51:13 +0300

    Finished styling

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -7,7 +7,9 @@ import { AiFillPlusCircle } from 'react-icons/ai';
 import { selectUser } from '../../redux/auth/authSelectors.js';
 import {
   Avatar,
+  BoxForm,
   BtnUserForm,
+  FormaBox,
   IconDiv,
   InputHide,
   InputUserForm,
@@ -15,6 +17,7 @@ import {
   User,
   UserFormBox,
   UserName,
+  WhiteBox,
 } from './UserForm.styled.jsx';
 
 const UserForm = () => {
@@ -48,83 +51,85 @@ const UserForm = () => {
   const dateMoment = moment(nowDate).format('YYYY-MM-DD');
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <FormaBox onSubmit={formik.handleSubmit}>
       <UserFormBox>
-        <label htmlFor="avatar">
-          <Avatar src={isUser.avatarURL} alt="User avatar" />
-          <IconDiv>
-            <AiFillPlusCircle size={14} stroke="#ffffff" fill="#3E85F3" />
-          </IconDiv>
+        <div>
+          <label htmlFor="avatar">
+            <Avatar src={isUser.avatarURL} alt="User avatar" />
+            <IconDiv>
+              <AiFillPlusCircle size={24} fill="#3E85F3" />
+            </IconDiv>
+            <WhiteBox></WhiteBox>
+            <InputHide type="file" name="avatar" accept="image/*" />
+          </label>
+          <UserName>{isUser.username ?? 'User Name'}</UserName>
 
-          <InputHide type="file" name="avatar" accept="image/*" />
-        </label>
-        <UserName>{isUser.username ?? 'User Name'}</UserName>
+          <User>User</User>
+        </div>
+        <BoxForm>
+          <LabelUserForm htmlFor="usrName">User Name</LabelUserForm>
+          <InputUserForm
+            id="usrName"
+            name="usrName"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.usrName}
+          />
+          {formik.touched.usrName && formik.errors.usrName ? (
+            <div>{formik.errors.usrName}</div>
+          ) : null}
 
-        <User>User</User>
+          <LabelUserForm htmlFor="birthday">Birthday</LabelUserForm>
+          <InputUserForm
+            id="birthday"
+            name="birthday"
+            type="birthday"
+            placeholder={dateMoment}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.birthday}
+          ></InputUserForm>
 
-        <LabelUserForm htmlFor="usrName">User Name</LabelUserForm>
-        <InputUserForm
-          id="usrName"
-          name="usrName"
-          type="text"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.usrName}
-        />
-        {formik.touched.usrName && formik.errors.usrName ? (
-          <div>{formik.errors.usrName}</div>
-        ) : null}
+          <LabelUserForm htmlFor="email">Email</LabelUserForm>
+          <InputUserForm
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Add your email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
 
-        <LabelUserForm htmlFor="phone">Phone</LabelUserForm>
-        <InputUserForm
-          id="phone"
-          name="phone"
-          type="phone"
-          placeholder="Add a phone number"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.phone}
-        ></InputUserForm>
+          <LabelUserForm htmlFor="phone">Phone</LabelUserForm>
+          <InputUserForm
+            id="phone"
+            name="phone"
+            type="phone"
+            placeholder="Add a phone number"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.phone}
+          ></InputUserForm>
 
-        <LabelUserForm htmlFor="birthday">Birthday</LabelUserForm>
-        <InputUserForm
-          id="birthday"
-          name="birthday"
-          type="birthday"
-          placeholder={dateMoment}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.birthday}
-        ></InputUserForm>
-
-        <LabelUserForm htmlFor="skype">Skype</LabelUserForm>
-        <InputUserForm
-          id="skype"
-          name="skype"
-          type="skype"
-          placeholder="Add a skype number"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.skype}
-        ></InputUserForm>
-
-        <LabelUserForm htmlFor="email">Email</LabelUserForm>
-        <InputUserForm
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Add your email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <div>{formik.errors.email}</div>
-        ) : null}
-
+          <LabelUserForm htmlFor="skype">Skype</LabelUserForm>
+          <InputUserForm
+            id="skype"
+            name="skype"
+            type="skype"
+            placeholder="Add a skype number"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.skype}
+          ></InputUserForm>
+        </BoxForm>
         <BtnUserForm type="submit">Save changes</BtnUserForm>
       </UserFormBox>
-    </form>
+    </FormaBox>
   );
 };
 
