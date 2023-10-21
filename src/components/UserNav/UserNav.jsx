b20429de7522c561e07a307b6764da6791fbe05f
Stanislav-i <itandeducationua@gmail.com>
2023-10-21 15:41:40 +0300

    sidebar dark theme

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -10,16 +10,21 @@ import {
 } from './UserNav.styled';
 import sprite from '../Pictures/sprite.svg';
 import menuGoose from '../Pictures/menu-goose.svg';
+import menuGooseDark from '../Pictures/menu-goose-dark.svg'
 
 import { getCurrentDate } from 'utils/calendar';
+import { useSelector } from 'react-redux';
+import { selectTheme } from 'redux/header/headerSlice';
 
 const UserNav = ({ onCloseMenu }) => {
   const currentDate = getCurrentDate();
+  const theme = useSelector(selectTheme);
 
   return (
     <UserNavBox>
-      <LogoBox>
-        <MenuIcon src={menuGoose} alt="menu goose"></MenuIcon>
+      <LogoBox color={theme === 'dark' ? '#fff' : '#3e85f3'}>
+        {/* <MenuIcon src={menuGoose} alt="menu goose"></MenuIcon> */}
+        <MenuIcon src={theme === 'dark' ? menuGooseDark : menuGoose} alt="menu goose"></MenuIcon>
         <p>
           G<em>oo</em>seTrack
         </p>
@@ -32,7 +37,16 @@ const UserNav = ({ onCloseMenu }) => {
       <NavTitle>User Panel</NavTitle>
       <ul>
         <NavItem>
-          <StyledNavLink to="/account">
+          <StyledNavLink
+            color={theme === 'dark' ? '#fff' : 'rgba(52, 52, 52, 0.5)'}
+            activecolor={theme === 'dark' ? '#fff' : '#3e85f3'}
+            bgactivecolor={
+              theme === 'dark'
+                ? 'var(--color-button-blue)'
+                : '#e3f3ff'
+            }
+            to="/account"
+          >
             <span>
               <svg width="24" height="24">
                 <use href={sprite + '#icon-user'}></use>
@@ -43,7 +57,16 @@ const UserNav = ({ onCloseMenu }) => {
         </NavItem>
 
         <NavItem>
-          <StyledNavLink to={`/calendar/month/${currentDate}`}>
+          <StyledNavLink
+            color={theme === 'dark' ? '#fff' : 'rgba(52, 52, 52, 0.5)'}
+            activecolor={theme === 'dark' ? '#fff' : '#3e85f3'}
+            bgactivecolor={
+              theme === 'dark'
+                ? 'var(--color-button-blue)'
+                : '#e3f3ff'
+            }
+            to={`/calendar/month/${currentDate}`}
+          >
             {/* <StyledNavLink to={`/calendar/month/2023-12-31`}> */}
             <span>
               <svg width="24" height="24">
@@ -55,7 +78,16 @@ const UserNav = ({ onCloseMenu }) => {
         </NavItem>
 
         <NavItem>
-          <StyledNavLink to="/statistics">
+          <StyledNavLink
+            color={theme === 'dark' ? '#fff' : 'rgba(52, 52, 52, 0.5)'}
+            activecolor={theme === 'dark' ? '#fff' : '#3e85f3'}
+            bgactivecolor={
+              theme === 'dark'
+                ? 'var(--color-button-blue)'
+                : '#e3f3ff'
+            }
+            to="/statistics"
+          >
             <span>
               <svg width="24" height="24">
                 <use href={sprite + '#icon-statistics'}></use>
