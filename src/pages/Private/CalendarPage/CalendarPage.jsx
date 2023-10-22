2a3d0f0cafa0cd5323806d705c04a0048d04c9b5
vladislav-slobodenjuk <vladjun@gmail.com>
2023-10-22 22:55:27 +0300

    clean comments

--- src/pages/Private/CalendarPage/CalendarPage.jsx
+++ src/pages/Private/CalendarPage/CalendarPage.jsx
@@ -1,12 +1,6 @@
 import { Suspense, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-  // useParams,
-} from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 import { addDays, addMonths, format, subDays, subMonths } from 'date-fns';
 
@@ -21,41 +15,14 @@ import ChoosedDay from 'components/ChoosedDay/ChoosedDay.jsx';
 
 import { getTasks as getTasksThunk } from 'redux/task/taskOperations';
 import { selectTheme } from 'redux/header/headerSlice.js';
-// import { selectTasks } from 'redux/task/taskSelectors';
 
 const CalendarPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const theme = useSelector(selectTheme);
-  // const tasks = useSelector(selectTasks);
 
-  // const currentDate = Object.values(useParams())[0].slice(-10);
   const currentDate = pathname.slice(-10);
-  console.log('currentDate: ', currentDate);
-  // const requestDate = currentDate.slice(0, 7);
-
-  // const [period, setPeriod] = useState('month');
-  // const currentPeriod = pathname.includes('month') ? 'month' : 'day';
-
-  // console.log('period: ', period);
-  // console.log('currentDate: ', currentDate);
-  // console.log('requestDate: ', requestDate);
-  // console.log('tasks: ', tasks);
-  // console.log(theme);
-
-  // useEffect(() => {
-  //   if (period !== currentPeriod) {
-  //     setPeriod(currentPeriod);
-  //   }
-  //   dispatch(getTasksThunk(requestDate));
-  // }, [currentPeriod, period, requestDate, dispatch]);
-
-  // useEffect(() => {
-  //   if (tasks.length > 0) return;
-
-  //   dispatch(getTasksThunk(requestDate));
-  // }, [dispatch, requestDate, tasks.length]);
   const prevMonthRef = useRef(null);
 
   useEffect(() => {
@@ -72,7 +39,6 @@ const CalendarPage = () => {
     if (pathname.includes('day')) {
       const newDate = subDays(new Date(currentDate), 1);
       navigate(`/calendar/day/${format(newDate, 'yyyy-MM-dd')}`);
-
       return;
     }
 
@@ -84,7 +50,6 @@ const CalendarPage = () => {
     if (pathname.includes('day')) {
       const newDate = addDays(new Date(currentDate), 1);
       navigate(`/calendar/day/${format(newDate, 'yyyy-MM-dd')}`);
-
       return;
     }
 
