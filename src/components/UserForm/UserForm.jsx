7420121c1d39d3a2288178d75a922e1f4046e8bb
olishkevych <olishkevych.natalia@gmail.com>
2023-10-22 14:38:55 +0200

    userform validation and styling

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -3,14 +3,15 @@ import { format } from 'date-fns';
 import { AiFillPlusCircle } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik, useFormik } from 'formik';
-import ValidSchema from 'helpers/schemaYap.js';
+import ValidSchema from 'helpers/ValidSchema.js';
 import { update } from 'redux/auth/authOperations.js';
-
 import { selectUser } from '../../redux/auth/authSelectors.js';
 import {
   Avatar,
   BoxForm,
   BtnUserForm,
+  Error,
+  FieldWrap,
   FormaBox,
   IconDiv,
   InputUserForm,
@@ -45,18 +46,17 @@ const UserForm = () => {
     },
     onSubmit: values => {
       var imagefile = document.getElementById('avatar');
-      //console.log(imagefile);
       updateUser({ ...values, avatarURL: imagefile });
     },
+    validationSchema: ValidSchema,
   });
   //const nowDate = new Date();
   const dateMoment = format(new Date(), 'MM/dd/yyyy');
 
   return (
-    <Formik validationSchema={ValidSchema}>
+    <Formik>
       <FormaBox onSubmit={formik.handleSubmit}>
         <UserFormBox
-          validationSchema={ValidSchema}
           bg={theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'}
           color={theme === 'dark' ? 'var(--color-field-names-dark)' : '#000'}
         >
@@ -103,110 +103,155 @@ const UserForm = () => {
             </User>
           </div>
           <BoxForm>
-            <LabelUserForm htmlFor="usrName">User Name</LabelUserForm>
-            <InputUserForm
-              bg={
-                theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'
-              }
-              color={theme === 'dark' ? '#fff' : '#000'}
-              bordercolor={
-                theme === 'dark'
-                  ? 'var(--color-field-names-dark)'
-                  : 'rgba(17, 17, 17, 0.1)'
-              }
-              id="usrName"
-              name="usrName"
-              type="text"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.usrName}
-            />
-            {formik.touched.usrName && formik.errors.usrName ? (
-              <div>{formik.errors.usrName}</div>
-            ) : null}
-
-            <LabelUserForm htmlFor="birthday">Birthday</LabelUserForm>
-            <InputUserForm
-              bg={
-                theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'
-              }
-              color={theme === 'dark' ? '#fff' : '#000'}
-              bordercolor={
-                theme === 'dark'
-                  ? 'var(--color-field-names-dark)'
-                  : 'rgba(17, 17, 17, 0.1)'
-              }
-              id="birthday"
-              name="birthday"
-              type="birthday"
-              placeholder={dateMoment}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.birthday}
-            ></InputUserForm>
-
-            <LabelUserForm htmlFor="email">Email</LabelUserForm>
-            <InputUserForm
-              bg={
-                theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'
-              }
-              color={theme === 'dark' ? '#fff' : '#000'}
-              bordercolor={
-                theme === 'dark'
-                  ? 'var(--color-field-names-dark)'
-                  : 'rgba(17, 17, 17, 0.1)'
-              }
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Add your email"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.email}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <div>{formik.errors.email}</div>
-            ) : null}
-
-            <LabelUserForm htmlFor="phone">Phone</LabelUserForm>
-            <InputUserForm
-              bg={
-                theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'
-              }
-              color={theme === 'dark' ? '#fff' : '#000'}
-              bordercolor={
-                theme === 'dark'
-                  ? 'var(--color-field-names-dark)'
-                  : 'rgba(17, 17, 17, 0.1)'
-              }
-              id="phone"
-              name="phone"
-              type="phone"
-              placeholder="Add a phone number"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.phone}
-            ></InputUserForm>
-
-            <LabelUserForm htmlFor="skype">Skype</LabelUserForm>
-            <InputUserForm
-              bg={
-                theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'
-              }
-              color={theme === 'dark' ? '#fff' : '#000'}
-              bordercolor={
-                theme === 'dark'
-                  ? 'var(--color-field-names-dark)'
-                  : 'rgba(17, 17, 17, 0.1)'
-              }
-              id="skype"
-              name="skype"
-              type="skype"
-              placeholder="Add a skype number"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.skype}
-            ></InputUserForm>
+            <FieldWrap>
+              <LabelUserForm htmlFor="usrName">User Name</LabelUserForm>
+              <InputUserForm
+                bg={
+                  theme === 'dark'
+                    ? 'var(--color-choice-dark-no-active)'
+                    : '#fff'
+                }
+                color={theme === 'dark' ? '#fff' : '#000'}
+                bordercolor={
+                  theme === 'dark'
+                    ? 'var(--color-field-names-dark)'
+                    : 'rgba(17, 17, 17, 0.1)'
+                }
+                id="usrName"
+                name="usrName"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.usrName}
+                style={
+                  formik.touched.usrName &&
+                  formik.errors.usrName && { border: '1px solid red' }
+                }
+              />
+              {formik.touched.usrName && formik.errors.usrName ? (
+                <Error>{formik.errors.usrName}</Error>
+              ) : null}
+            </FieldWrap>
+            <FieldWrap>
+              <LabelUserForm htmlFor="birthday">Birthday</LabelUserForm>
+              <InputUserForm
+                bg={
+                  theme === 'dark'
+                    ? 'var(--color-choice-dark-no-active)'
+                    : '#fff'
+                }
+                color={theme === 'dark' ? '#fff' : '#000'}
+                bordercolor={
+                  theme === 'dark'
+                    ? 'var(--color-field-names-dark)'
+                    : 'rgba(17, 17, 17, 0.1)'
+                }
+                id="birthday"
+                name="birthday"
+                type="birthday"
+                placeholder={dateMoment}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.birthday}
+                style={
+                  formik.touched.birthday &&
+                  formik.errors.birthday && { border: '1px solid red' }
+                }
+              ></InputUserForm>
+              {formik.touched.birthday && formik.errors.birthday ? (
+                <Error>{formik.errors.birthday}</Error>
+              ) : null}
+            </FieldWrap>
+            <FieldWrap>
+              <LabelUserForm htmlFor="email">Email</LabelUserForm>
+              <InputUserForm
+                bg={
+                  theme === 'dark'
+                    ? 'var(--color-choice-dark-no-active)'
+                    : '#fff'
+                }
+                color={theme === 'dark' ? '#fff' : '#000'}
+                bordercolor={
+                  theme === 'dark'
+                    ? 'var(--color-field-names-dark)'
+                    : 'rgba(17, 17, 17, 0.1)'
+                }
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Add your email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                style={
+                  formik.touched.email &&
+                  formik.errors.email && { border: '1px solid red' }
+                }
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <Error>{formik.errors.email}</Error>
+              ) : null}
+            </FieldWrap>
+            <FieldWrap>
+              <LabelUserForm htmlFor="phone">Phone</LabelUserForm>
+              <InputUserForm
+                bg={
+                  theme === 'dark'
+                    ? 'var(--color-choice-dark-no-active)'
+                    : '#fff'
+                }
+                color={theme === 'dark' ? '#fff' : '#000'}
+                bordercolor={
+                  theme === 'dark'
+                    ? 'var(--color-field-names-dark)'
+                    : 'rgba(17, 17, 17, 0.1)'
+                }
+                style={
+                  formik.touched.phone &&
+                  formik.errors.phone && { border: '1px solid red' }
+                }
+                id="phone"
+                name="phone"
+                type="phone"
+                placeholder="Add a phone number"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.phone}
+              ></InputUserForm>
+              {formik.touched.phone && formik.errors.phone ? (
+                <Error>{formik.errors.phone}</Error>
+              ) : null}
+            </FieldWrap>
+            <FieldWrap>
+              <LabelUserForm htmlFor="skype">Skype</LabelUserForm>
+              <InputUserForm
+                bg={
+                  theme === 'dark'
+                    ? 'var(--color-choice-dark-no-active)'
+                    : '#fff'
+                }
+                color={theme === 'dark' ? '#fff' : '#000'}
+                bordercolor={
+                  theme === 'dark'
+                    ? 'var(--color-field-names-dark)'
+                    : 'rgba(17, 17, 17, 0.1)'
+                }
+                id="skype"
+                name="skype"
+                type="skype"
+                placeholder="Add a skype number"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.skype}
+                style={
+                  formik.touched.skype &&
+                  formik.errors.skype && { border: '1px solid red' }
+                }
+              ></InputUserForm>
+              {formik.touched.skype && formik.errors.skype ? (
+                <Error>{formik.errors.skype}</Error>
+              ) : null}{' '}
+            </FieldWrap>
           </BoxForm>
           <BtnUserForm type="submit">Save changes</BtnUserForm>
         </UserFormBox>
