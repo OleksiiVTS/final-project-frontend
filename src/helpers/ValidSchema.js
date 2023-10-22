efce8cce8b9511ab881a0b1ff0762ddf1d1b339e
olishkevych <olishkevych.natalia@gmail.com>
2023-10-22 15:35:35 +0200

    adding skype validation

--- src/helpers/ValidSchema.js
+++ src/helpers/ValidSchema.js
@@ -5,16 +5,20 @@ const phoneRegExp =
 const birthdayRegExp =
   '^(0[1-9]|[12]\\d|3[01])/(0[1-9]|1[0-2])/(19\\d{2}|20\\d{2})$';
 
+const skypeRegExp = /^[a-z][a-z0-9.,\-_]{5,31}$/i;
+
 const ValidSchema = Yup.object().shape({
   usrName: Yup.string()
     .max(16, 'Must be 16 characters or less')
     .required('Username is required'),
-  phone: Yup.string().matches(phoneRegExp, 'Wrong number'),
+  phone: Yup.string().matches(phoneRegExp, 'Please check your input'),
   birthday: Yup.string().matches(
     birthdayRegExp,
     'Wrong format. Example: 22/10/2022'
   ),
-  skype: Yup.string().max(16, 'Must be 16 characters or less'),
+  skype: Yup.string()
+    .matches(skypeRegExp, 'Please check your input')
+    .max(16, 'Must be 16 characters or less'),
   email: Yup.string()
     .email('Invalid email address')
     .required('Email is required'),
