839eda4f4f78a46e942b27a7e96e4391f24ffe21
olishkevych <olishkevych.natalia@gmail.com>
2023-10-22 18:51:16 +0200

    adding modal for submitting account details

--- src/components/DeleteForm/DeleteForm.jsx
+++ src/components/DeleteForm/DeleteForm.jsx
@@ -1,17 +1,18 @@
 import { DeleteBtn, CancelBtn, BtnWrapper, TextDiv } from './DeleteForm.styled';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import sprite from '../Pictures/sprite.svg';
 import { deleteTask } from 'redux/task/taskOperations';
 import Checkmark from 'components/TaskForm/Checkmark/Checkmark';
 import { useState } from 'react';
+import { selectTheme } from 'redux/header/headerSlice';
 
 const DeleteForm = ({ task, closeModal }) => {
   const dispatch = useDispatch();
+  const theme = useSelector(selectTheme);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const removeTask = () => {
     setIsSubmitted(true);
-
     setTimeout(() => closeModal(), 1500);
     setTimeout(() => dispatch(deleteTask(task._id)), 1500);
   };
@@ -21,7 +22,11 @@ const DeleteForm = ({ task, closeModal }) => {
         <Checkmark />
       ) : (
         <>
-          <TextDiv>Do you want to delete the task?</TextDiv>
+          <TextDiv
+            style={theme === 'dark' ? { color: '#fff' } : { color: '#000' }}
+          >
+            Do you want to delete the task?
+          </TextDiv>
           <BtnWrapper>
             <DeleteBtn onClick={removeTask}>
               <svg width="18" height="18" style={{ marginRight: 8 }}>
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -24,12 +24,14 @@ import {
 import { useRef } from 'react';
 import PreviewAvatar from './PreviewAvatar.js';
 import { selectTheme } from 'redux/header/headerSlice.js';
+import { useState } from 'react';
+import TaskModal from 'components/TaskModal/TaskModal.jsx';
+import ConfirmationModal from './ConfirmationModal/ConfirmationModal.jsx';
 
 const UserForm = () => {
-  const dispatch = useDispatch();
-  const updateUser = updateUser => {
-    dispatch(update(updateUser));
-  };
+  const [showModal, setShowModal] = useState(false);
+  const [formValues, setFormValues] = useState(false);
+
   const isUser = useSelector(selectUser);
   const theme = useSelector(selectTheme);
 
@@ -45,218 +47,228 @@ const UserForm = () => {
       file: null,
     },
     onSubmit: values => {
+      setShowModal(true);
       var imagefile = document.getElementById('avatar');
-      updateUser({ ...values, avatarURL: imagefile });
+      setFormValues({ ...values, avatarURL: imagefile });
     },
     validationSchema: ValidSchema,
   });
+
+  const closeModal = () => setShowModal(false);
   //const nowDate = new Date();
   const dateMoment = format(new Date(), 'MM/dd/yyyy');
 
   return (
-    <Formik>
-      <FormaBox onSubmit={formik.handleSubmit}>
-        <UserFormBox
-          bg={theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'}
-          color={theme === 'dark' ? 'var(--color-field-names-dark)' : '#000'}
-        >
-          <div>
-            <label htmlFor="avatar">
-              {formik.values.file ? (
-                <PreviewAvatar file={formik.values.file} />
-              ) : (
-                <Avatar src={isUser.avatarURL} alt="User avatar" />
-              )}
+    <>
+      <Formik>
+        <FormaBox onSubmit={formik.handleSubmit}>
+          <UserFormBox
+            bg={
+              theme === 'dark' ? 'var(--color-choice-dark-no-active)' : '#fff'
+            }
+            color={theme === 'dark' ? 'var(--color-field-names-dark)' : '#000'}
+          >
+            <div>
+              <label htmlFor="avatar">
+                {formik.values.file ? (
+                  <PreviewAvatar file={formik.values.file} />
+                ) : (
+                  <Avatar src={isUser.avatarURL} alt="User avatar" />
+                )}
 
-              <IconDiv
-                type="button"
-                onClick={() => {
-                  fileRef.current.click();
-                }}
+                <IconDiv
+                  type="button"
+                  onClick={() => {
+                    fileRef.current.click();
+                  }}
+                >
+                  <AiFillPlusCircle size={24} fill="#3E85F3" />
+                </IconDiv>
+                <WhiteBox></WhiteBox>
+                <input
+                  ref={fileRef}
+                  id="avatar"
+                  type="file"
+                  hidden
+                  accept="image/*"
+                  onChange={event => {
+                    formik.setFieldValue('file', event.target.files[0]);
+                  }}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.avatar}
+                />
+              </label>
+              <UserName color={theme === 'dark' ? '#fff' : '#343434'}>
+                {isUser.username ?? 'User Name'}
+              </UserName>
+
+              <User
+                color={
+                  theme === 'dark' ? 'var(--color-field-names-dark)' : '#343434'
+                }
               >
-                <AiFillPlusCircle size={24} fill="#3E85F3" />
-              </IconDiv>
-              <WhiteBox></WhiteBox>
-              <input
-                ref={fileRef}
-                id="avatar"
-                type="file"
-                hidden
-                accept="image/*"
-                onChange={event => {
-                  formik.setFieldValue('file', event.target.files[0]);
-                }}
-                onBlur={formik.handleBlur}
-                value={formik.values.avatar}
-              />
-            </label>
-            <UserName color={theme === 'dark' ? '#fff' : '#343434'}>
-              {isUser.username ?? 'User Name'}
-            </UserName>
-
-            <User
-              color={
-                theme === 'dark' ? 'var(--color-field-names-dark)' : '#343434'
-              }
-            >
-              User
-            </User>
-          </div>
-          <BoxForm>
-            <FieldWrap>
-              <LabelUserForm htmlFor="usrName">User Name</LabelUserForm>
-              <InputUserForm
-                bg={
-                  theme === 'dark'
-                    ? 'var(--color-choice-dark-no-active)'
-                    : '#fff'
-                }
-                color={theme === 'dark' ? '#fff' : '#000'}
-                bordercolor={
-                  theme === 'dark'
-                    ? 'var(--color-field-names-dark)'
-                    : 'rgba(17, 17, 17, 0.1)'
-                }
-                id="usrName"
-                name="usrName"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.usrName}
-                style={
-                  formik.touched.usrName &&
-                  formik.errors.usrName && { border: '1px solid red' }
-                }
-              />
-              {formik.touched.usrName && formik.errors.usrName ? (
-                <Error>{formik.errors.usrName}</Error>
-              ) : null}
-            </FieldWrap>
-            <FieldWrap>
-              <LabelUserForm htmlFor="birthday">Birthday</LabelUserForm>
-              <InputUserForm
-                bg={
-                  theme === 'dark'
-                    ? 'var(--color-choice-dark-no-active)'
-                    : '#fff'
-                }
-                color={theme === 'dark' ? '#fff' : '#000'}
-                bordercolor={
-                  theme === 'dark'
-                    ? 'var(--color-field-names-dark)'
-                    : 'rgba(17, 17, 17, 0.1)'
-                }
-                id="birthday"
-                name="birthday"
-                type="birthday"
-                placeholder={dateMoment}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.birthday}
-                style={
-                  formik.touched.birthday &&
-                  formik.errors.birthday && { border: '1px solid red' }
-                }
-              ></InputUserForm>
-              {formik.touched.birthday && formik.errors.birthday ? (
-                <Error>{formik.errors.birthday}</Error>
-              ) : null}
-            </FieldWrap>
-            <FieldWrap>
-              <LabelUserForm htmlFor="email">Email</LabelUserForm>
-              <InputUserForm
-                bg={
-                  theme === 'dark'
-                    ? 'var(--color-choice-dark-no-active)'
-                    : '#fff'
-                }
-                color={theme === 'dark' ? '#fff' : '#000'}
-                bordercolor={
-                  theme === 'dark'
-                    ? 'var(--color-field-names-dark)'
-                    : 'rgba(17, 17, 17, 0.1)'
-                }
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Add your email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-                style={
-                  formik.touched.email &&
-                  formik.errors.email && { border: '1px solid red' }
-                }
-              />
-              {formik.touched.email && formik.errors.email ? (
-                <Error>{formik.errors.email}</Error>
-              ) : null}
-            </FieldWrap>
-            <FieldWrap>
-              <LabelUserForm htmlFor="phone">Phone</LabelUserForm>
-              <InputUserForm
-                bg={
-                  theme === 'dark'
-                    ? 'var(--color-choice-dark-no-active)'
-                    : '#fff'
-                }
-                color={theme === 'dark' ? '#fff' : '#000'}
-                bordercolor={
-                  theme === 'dark'
-                    ? 'var(--color-field-names-dark)'
-                    : 'rgba(17, 17, 17, 0.1)'
-                }
-                style={
-                  formik.touched.phone &&
-                  formik.errors.phone && { border: '1px solid red' }
-                }
-                id="phone"
-                name="phone"
-                type="phone"
-                placeholder="Add a phone number"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.phone}
-              ></InputUserForm>
-              {formik.touched.phone && formik.errors.phone ? (
-                <Error>{formik.errors.phone}</Error>
-              ) : null}
-            </FieldWrap>
-            <FieldWrap>
-              <LabelUserForm htmlFor="skype">Skype</LabelUserForm>
-              <InputUserForm
-                bg={
-                  theme === 'dark'
-                    ? 'var(--color-choice-dark-no-active)'
-                    : '#fff'
-                }
-                color={theme === 'dark' ? '#fff' : '#000'}
-                bordercolor={
-                  theme === 'dark'
-                    ? 'var(--color-field-names-dark)'
-                    : 'rgba(17, 17, 17, 0.1)'
-                }
-                id="skype"
-                name="skype"
-                type="skype"
-                placeholder="Add a skype number"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.skype}
-                style={
-                  formik.touched.skype &&
-                  formik.errors.skype && { border: '1px solid red' }
-                }
-              ></InputUserForm>
-              {formik.touched.skype && formik.errors.skype ? (
-                <Error>{formik.errors.skype}</Error>
-              ) : null}{' '}
-            </FieldWrap>
-          </BoxForm>
-          <BtnUserForm type="submit">Save changes</BtnUserForm>
-        </UserFormBox>
-      </FormaBox>
-    </Formik>
+                User
+              </User>
+            </div>
+            <BoxForm>
+              <FieldWrap>
+                <LabelUserForm htmlFor="usrName">User Name</LabelUserForm>
+                <InputUserForm
+                  bg={
+                    theme === 'dark'
+                      ? 'var(--color-choice-dark-no-active)'
+                      : '#fff'
+                  }
+                  color={theme === 'dark' ? '#fff' : '#000'}
+                  bordercolor={
+                    theme === 'dark'
+                      ? 'var(--color-field-names-dark)'
+                      : 'rgba(17, 17, 17, 0.1)'
+                  }
+                  id="usrName"
+                  name="usrName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.usrName}
+                  style={
+                    formik.touched.usrName &&
+                    formik.errors.usrName && { border: '1px solid red' }
+                  }
+                />
+                {formik.touched.usrName && formik.errors.usrName ? (
+                  <Error>{formik.errors.usrName}</Error>
+                ) : null}
+              </FieldWrap>
+              <FieldWrap>
+                <LabelUserForm htmlFor="birthday">Birthday</LabelUserForm>
+                <InputUserForm
+                  bg={
+                    theme === 'dark'
+                      ? 'var(--color-choice-dark-no-active)'
+                      : '#fff'
+                  }
+                  color={theme === 'dark' ? '#fff' : '#000'}
+                  bordercolor={
+                    theme === 'dark'
+                      ? 'var(--color-field-names-dark)'
+                      : 'rgba(17, 17, 17, 0.1)'
+                  }
+                  id="birthday"
+                  name="birthday"
+                  type="birthday"
+                  placeholder={dateMoment}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.birthday}
+                  style={
+                    formik.touched.birthday &&
+                    formik.errors.birthday && { border: '1px solid red' }
+                  }
+                ></InputUserForm>
+                {formik.touched.birthday && formik.errors.birthday ? (
+                  <Error>{formik.errors.birthday}</Error>
+                ) : null}
+              </FieldWrap>
+              <FieldWrap>
+                <LabelUserForm htmlFor="email">Email</LabelUserForm>
+                <InputUserForm
+                  bg={
+                    theme === 'dark'
+                      ? 'var(--color-choice-dark-no-active)'
+                      : '#fff'
+                  }
+                  color={theme === 'dark' ? '#fff' : '#000'}
+                  bordercolor={
+                    theme === 'dark'
+                      ? 'var(--color-field-names-dark)'
+                      : 'rgba(17, 17, 17, 0.1)'
+                  }
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Add your email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  style={
+                    formik.touched.email &&
+                    formik.errors.email && { border: '1px solid red' }
+                  }
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <Error>{formik.errors.email}</Error>
+                ) : null}
+              </FieldWrap>
+              <FieldWrap>
+                <LabelUserForm htmlFor="phone">Phone</LabelUserForm>
+                <InputUserForm
+                  bg={
+                    theme === 'dark'
+                      ? 'var(--color-choice-dark-no-active)'
+                      : '#fff'
+                  }
+                  color={theme === 'dark' ? '#fff' : '#000'}
+                  bordercolor={
+                    theme === 'dark'
+                      ? 'var(--color-field-names-dark)'
+                      : 'rgba(17, 17, 17, 0.1)'
+                  }
+                  style={
+                    formik.touched.phone &&
+                    formik.errors.phone && { border: '1px solid red' }
+                  }
+                  id="phone"
+                  name="phone"
+                  type="phone"
+                  placeholder="Add a phone number"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.phone}
+                ></InputUserForm>
+                {formik.touched.phone && formik.errors.phone ? (
+                  <Error>{formik.errors.phone}</Error>
+                ) : null}
+              </FieldWrap>
+              <FieldWrap>
+                <LabelUserForm htmlFor="skype">Skype</LabelUserForm>
+                <InputUserForm
+                  bg={
+                    theme === 'dark'
+                      ? 'var(--color-choice-dark-no-active)'
+                      : '#fff'
+                  }
+                  color={theme === 'dark' ? '#fff' : '#000'}
+                  bordercolor={
+                    theme === 'dark'
+                      ? 'var(--color-field-names-dark)'
+                      : 'rgba(17, 17, 17, 0.1)'
+                  }
+                  id="skype"
+                  name="skype"
+                  type="skype"
+                  placeholder="Add a skype number"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.skype}
+                  style={
+                    formik.touched.skype &&
+                    formik.errors.skype && { border: '1px solid red' }
+                  }
+                ></InputUserForm>
+                {formik.touched.skype && formik.errors.skype ? (
+                  <Error>{formik.errors.skype}</Error>
+                ) : null}{' '}
+              </FieldWrap>
+            </BoxForm>
+            <BtnUserForm type="submit">Save changes</BtnUserForm>
+          </UserFormBox>
+        </FormaBox>
+      </Formik>
+      {showModal && (
+        <ConfirmationModal values={formValues} closeModal={closeModal} />
+      )}
+    </>
   );
 };
 
