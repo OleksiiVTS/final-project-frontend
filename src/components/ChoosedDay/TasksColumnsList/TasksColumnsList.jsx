9b8109e37c0d6f31506b97862d67fee717361095
vladislav-slobodenjuk <vladjun@gmail.com>
2023-10-23 13:26:56 +0300

    swiper

--- src/components/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
+++ src/components/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
@@ -4,17 +4,48 @@ import TasksColumnItem from '../TasksColumnItem/TasksColumnItem';
 import { parseCategoryTitle } from 'helpers/helpers';
 import { CATEGORY_LIST } from 'constants/categoryList';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+
 const TasksColumnsList = ({ sortedTasks }) => {
   return (
     <StyledTasksColumnsList className="TasksColumnsList">
-      {CATEGORY_LIST.map(category => (
-        <TasksColumnItem
-          key={category}
-          title={parseCategoryTitle(category)}
-          category={category}
-          tasks={sortedTasks[category]}
-        />
-      ))}
+      <Swiper
+        // spaceBetween={10}
+        // slidesPerView={1}
+        breakpoints={{
+          // when window width is >= 640px
+          // 375: {
+          //   width: 375,
+          //   slidesPerView: 1,
+          // },
+          // when window width is >= 768px
+          768: {
+            width: 768,
+            slidesPerView: 2,
+          },
+          1440: {
+            width: 768,
+            slidesPerView: 2,
+          },
+        }}
+        // slidesPerView={true}
+        width={375}
+      >
+        {CATEGORY_LIST.map(category => (
+          <SwiperSlide key={category}>
+            <TasksColumnItem
+              title={parseCategoryTitle(category)}
+              category={category}
+              tasks={sortedTasks[category]}
+            />
+          </SwiperSlide>
+        ))}
+        {/* <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide> */}
+      </Swiper>
     </StyledTasksColumnsList>
   );
 };
--- src/components/ChoosedDay/TasksColumnsList/TasksColumnsList.styled.js
+++ src/components/ChoosedDay/TasksColumnsList/TasksColumnsList.styled.js
@@ -2,19 +2,35 @@ import styled from 'styled-components';
 
 export const StyledTasksColumnsList = styled.div`
   display: flex;
-  column-gap: 20px;
+  /* column-gap: 20px; */
   /* flex-wrap: wrap; */
 
-  /* width: 335px; */
-  overflow-x: auto;
+  width: 335px;
+  /* overflow-x: auto; */
 
   @media screen and (min-width: 768px) {
     column-gap: 16px;
-    /* width: 704px; */
+    width: 704px;
   }
 
   @media screen and (min-width: 1440px) {
     column-gap: 27px;
-    /* width: 1086px; */
+    width: 1086px;
+  }
+
+  .swiper-container {
+    width: 480px;
+  }
+
+  @media screen and (min-width: 640px) {
+    .swiper-container {
+      width: 640px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .swiper-container {
+      width: 768px;
+    }
   }
 `;
