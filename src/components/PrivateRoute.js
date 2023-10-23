ec93b5e9b1b92e7162801c74d50f693c8017351c
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-10-23 22:28:52 +0300

    fix rout and size calendar

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { selectIsRefreshing, selectLoggedIn } from 'redux/auth/authSelectors';
+import { selectToken } from 'redux/auth/authSelectors';
 
 const PrivateRoute = () => {
-  const loggedIn = useSelector(selectLoggedIn);
-  const IsRefreshing = useSelector(selectIsRefreshing);
-  console.log(loggedIn);
-  return !IsRefreshing ?? loggedIn ? <Outlet /> : <Navigate to={'/login'} />;
+  const IsToken = useSelector(selectToken);
+  return IsToken ? <Outlet /> : <Navigate to={'/login'} />;
 };
 
 export default PrivateRoute;
