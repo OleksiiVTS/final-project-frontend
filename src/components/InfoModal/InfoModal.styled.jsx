ba83c51dc9df7b1ca7a8d01a358bc1b246df8c14
Rogok-Serg <rogoksv@gmail.com>
2023-10-24 12:33:09 +0300

    +

---
+++ src/components/InfoModal/InfoModal.styled.jsx
@@ -0,0 +1,63 @@
+import styled from '@emotion/styled/macro';
+
+export const ConfirmBtn = styled.button`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #3e85f3;
+  border-radius: 8px;
+  padding: 10px;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1.28;
+  border: 0;
+  cursor: pointer;
+
+  &:hover {
+    background: #2b78ef;
+    box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+    transition: background-color box-shadow 0.2s;
+  }
+`;
+
+export const CancelBtn = styled.button`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #efefef;
+  border-radius: 8px;
+  padding: 10px;
+  color: #111;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1.28;
+  border: 0;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+    transition: box-shadow 0.2s;
+  }
+`;
+
+export const BtnWrapper = styled.div`
+  display: flex;
+  column-gap: 14px;
+  margin-bottom: 14px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextDiv = styled.div`
+  margin-top: 14px;
+  margin-bottom: 14px;
+  text-align: center;
+  color: #343434;
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 14px 0;
+`;
