cf6f5982e5e2e150b4c8303a6be21b49ce17d8f8
ishcenko <a_ishcenko@i.ua>
2023-10-24 20:57:55 +0300

    !!!!!!

---
+++ src/components/FooterTeam/Team/Team.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const FooterModalContainer = styled.div`
+  background: inherit;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 25px 20px 25px;
+`;
+
+export const FooterModalTeam = styled.h3`
+  position: absolute;
+  top: 20px;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 700;
+  color: #343434;
+`;
+
+export const SwiperContainer = styled.div`
+  width: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
