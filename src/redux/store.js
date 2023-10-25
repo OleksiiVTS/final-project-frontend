dda881b0db4c1649e63c3e3fad15b7910d3b71d8
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-10-25 11:42:11 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,56 +1,5 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { authReducer } from './auth/authSlice';
-import { reviewsReducer } from './review/reviewSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import { taskReducer } from './task/taskSlice';
-import { headerReducer } from './header/headerSlice';
-import storage from 'redux-persist/lib/storage';
-
-const ThemePersistConfig = {
-  key: 'theme',
-  storage,
-};
-
-const appReducer = combineReducers({
-  auth: authReducer,
-  tasks: taskReducer,
-  review: reviewsReducer,
-  header: persistReducer(ThemePersistConfig, headerReducer),
-});
-
-const rootReducer = (state, action) => {
-  if (action.type === 'auth/logout/fulfilled') {
-    state = undefined;
-  }
-  return appReducer(state, action);
-};
-
-export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
-
-export const persistor = persistStore(store);
-
-
-
-// import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore, combineReducers } from '@reduxjs/toolkit';
 // import { authReducer } from './auth/authSlice';
-// // import { tasksReducer } from "";
 // import { reviewsReducer } from './review/reviewSlice';
 // import {
 //   persistStore,
@@ -71,13 +20,22 @@ export const persistor = persistStore(store);
 //   storage,
 // };
 
+// const appReducer = combineReducers({
+//   auth: authReducer,
+//   tasks: taskReducer,
+//   review: reviewsReducer,
+//   header: persistReducer(ThemePersistConfig, headerReducer),
+// });
+
+// const rootReducer = (state, action) => {
+//   if (action.type === 'auth/logout/fulfilled') {
+//     state = undefined;
+//   }
+//   return appReducer(state, action);
+// };
+
 // export const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//     tasks: taskReducer,
-//     review: reviewsReducer,
-//     header: persistReducer(ThemePersistConfig, headerReducer),
-//   },
+//   reducer: rootReducer,
 //   middleware: getDefaultMiddleware =>
 //     getDefaultMiddleware({
 //       serializableCheck: {
@@ -87,3 +45,43 @@ export const persistor = persistStore(store);
 // });
 
 // export const persistor = persistStore(store);
+
+import { configureStore } from '@reduxjs/toolkit';
+import { authReducer } from './auth/authSlice';
+// import { tasksReducer } from "";
+import { reviewsReducer } from './review/reviewSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { taskReducer } from './task/taskSlice';
+import { headerReducer } from './header/headerSlice';
+import storage from 'redux-persist/lib/storage';
+
+const ThemePersistConfig = {
+  key: 'theme',
+  storage,
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    tasks: taskReducer,
+    review: reviewsReducer,
+    header: persistReducer(ThemePersistConfig, headerReducer),
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
